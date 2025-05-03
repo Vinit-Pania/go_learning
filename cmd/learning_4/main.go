2dc7d22b4582d0422a9fe0df434f3618b0e7592d
vinitpania <vinitpania26@gmail.com>
2025-05-04 01:16:55 +0530

    learnid about slice in go

--- cmd/learning_4/main.go
+++ cmd/learning_4/main.go
@@ -25,4 +25,25 @@ func main() {
 	intArr5 := [...]int32{1, 1122, 111222, 1112222, 111122222}
 	fmt.Println(intArr5)
 
+	//Learning Slice which is wrapper of aare
+
+	int_Slice1 := []int32{2, 21, 221, 2221, 2221}
+	fmt.Println(int_Slice1)
+	fmt.Println("The lebgth of the aare is %v and the capacity of the array is %v", len(int_Slice1), cap(int_Slice1))
+
+	int_Slice1 = append(int_Slice1, 222211)
+	fmt.Println(int_Slice1)
+	fmt.Println("\n The lebgth of the aare is %v and the capacity of the array is %v", len(int_Slice1), cap(int_Slice1))
+
+	//cant reAD THE value from the added part of the capacity
+	fmt.Println(int_Slice1[4])
+
+	var int_Slice2 []int32 = []int32{222222211, 2121212212}
+	int_Slice2 = append(int_Slice1, int_Slice2...)
+	fmt.Println(int_Slice1)
+
+	//another way makeing slice using slice which has lemgth and capacity default length == capacity
+	var int_Slice3 []int32 = make([]int32, 1, 10)
+	fmt.Println(int_Slice3)
+
 }
